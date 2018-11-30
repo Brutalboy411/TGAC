65b394502f7b264fab52694c935eeb5cea14ee10
Brutalboy411 <45496835+Brutalboy411@users.noreply.github.com>
2018-11-30 19:14:38 +0000

    dmsalmdsa

--- src/main/java/me/terror/tgac/MainClass.java
+++ src/main/java/me/terror/tgac/MainClass.java
@@ -1,17 +1,21 @@
 package me.terror.tgac;
 
+import me.terror.tgac.util.User;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.UUID;
 
 public final class MainClass extends JavaPlugin {
 
+    public static final HashMap<UUID, User> USER = new HashMap();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
         getServer().getConsoleSender().sendMessage("Aska suge pula");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
---
+++ src/main/java/me/terror/tgac/util/User.java
@@ -0,0 +1,8 @@
+package me.terror.tgac.util;
+
+import org.bukkit.entity.Player;
+
+public class User {
+
+    private final Player player;
+}
