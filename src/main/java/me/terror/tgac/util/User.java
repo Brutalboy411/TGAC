9f6c42f7048a35e27fa345cc4316be2e49cfdc9a
Brutalboy411 <45496835+Brutalboy411@users.noreply.github.com>
2018-11-30 19:32:34 +0000

    dsadsa

--- src/main/java/me/terror/tgac/util/User.java
+++ src/main/java/me/terror/tgac/util/User.java
@@ -2,7 +2,58 @@ package me.terror.tgac.util;
 
 import org.bukkit.entity.Player;
 
-public class User {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
-    private final Player player;
+public class User {
+    public Player player ;
+    public double oldY = 0.0D;
+    private ArrayList<Long> hits = new ArrayList<>();
+    private HashMap<Long, Integer> entities = new HashMap<>();
+    private long lastTimeHitsCleaned = 0L;
+    private long lastTimeEntitiesCleaned = 0L;
+    public boolean wasGoingUp = false;
+    public int oldYModifier = 0;
+    public int ticksUp = 0;
+    public int oldTicksUp = 0;
+
+    public User(Player player){
+        this.player = player;
+    }
+    public Player getPlayer(){
+        return this.player;
+    }
+
+    public void addHit(){
+        this.hits.add(Long.valueOf(System.currentTimeMillis()));
+    }
+
+    public int getHits(){
+        long start = System.currentTimeMillis();
+        ArrayList<Long> toRemove = new ArrayList<>();
+        int result = 0;
+        for (Iterator localIterator = this.hits.iterator(); localIterator.hasNext();){
+            long 1 = ((Long)localIterator.next()).longValue();
+            if (start - 1 > 1000L){
+                toRemove.add(Long.valueOf());
+            }else {
+                result++;
+            }
+        }
+        this.hits.removeAll(toRemove);
+        toRemove.clear();
+        this.lastTimeHitsCleaned = start;
+        return result;
+    }
+
+    public void addEntity(int i){
+        this.entities.put(Long.valueOf(System.currentTimeMillis()), Integer.valueOf(i));
+    }
+
+    public int getEntities(){
+        long start = System.currentTimeMillis();
+        ArrayList<Long>
+    }
+
 }
